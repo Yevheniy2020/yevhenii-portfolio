46fec9a14039150e55a79f1e9fdb2608b405301e
Yevhenii Mynenko <68693183+Yevheniy2020@users.noreply.github.com>
2024-08-11 23:59:00 +0300

    fixed build

--- src/app/components/pages/Article/ArticleContent1.component.jsx
+++ src/app/components/pages/Article/ArticleContent1.component.jsx
@@ -12,7 +12,7 @@ const ArticleContent1Component = () => {
                     <div className='w-full h-[2px] bg-gold mt-[18px] sm:mt-[12px]'/>
                     <div className='text-2xl pt-[24px] font-semibold sm:text-[18px] sm:pt-[12px]'>Introduction</div>
                     <div className='pt-[24px] sm:pt-[12px]'>Recently, I have been focusing on improving the SEO aspects of a particular online resource. A critical area that often causes difficulties is the optimization of images, which play a key role in both website performance and search engine rankings. Delving into the complexities of image optimization, I discovered one very interesting fact about svg and the img tag.</div>
-                    <div className='pt-[32px] sm:pt-[18px]'>Okay, let's go to some websites and look at svg.</div>
+                    <div className='pt-[32px] sm:pt-[18px]'>Okay, let&lsquo;s go to some websites and look at svg.</div>
                     <div className='pt-[18px] sm:pt-[14px]'>First site:</div>
                     <div  className='pt-[12px]'>
                         <Image src="/image/blog/articles/seo-svg-img/rozetka.jpg" width={680} height={380} alt='rozetka shop img logo' className='max-w-[680px] w-full  object-cover'/>
@@ -35,10 +35,10 @@ const ArticleContent1Component = () => {
                         In the two examples we can see the difference in resolution of our SVG, how can this affect SEO?
                     </div>
                     <div className='pt-[24px] sm:pt-[12px]'>
-                        Unfortunately, in the second example we can get a rather bad result. The point is that the search robot is very sensitive to media resources, in this example to images. It will first go through the pictures, determining their priority, it uses indicators such as relevance, resolution of the picture and how it is optimized. Now let's go back to our 2nd example and see that there's an SVG of a good format that can, firstly, be read by the search engine robot, secondly, be read as the main picture.
+                        Unfortunately, in the second example we can get a rather bad result. The point is that the search robot is very sensitive to media resources, in this example to images. It will first go through the pictures, determining their priority, it uses indicators such as relevance, resolution of the picture and how it is optimized. Now let&lsquo;s go back to our 2nd example and see that there&lsquo;s an SVG of a good format that can, firstly, be read by the search engine robot, secondly, be read as the main picture.
                     </div>
                     <div className='pt-[24px] sm:pt-[12px]'>
-                        Here's what google says about it
+                        Here&lsquo;s what google says about it
                     </div>
                     <div className='mt-[12px] p-[20px] bg-opacity-block relative sm:p-[12px]'>
                         <div className='mb-[12px]'>
@@ -70,7 +70,7 @@ const ArticleContent1Component = () => {
                     </div>
                     <div className='pt-[12px]'>A very quick and easy way to get the desired results. Here also google robots will not index our SVGs</div>
                     <div className='pt-[32px] font-semibold text-2xl sm:text-[14px] sm:pt-[18px]'>Conclusion</div>
-                    <div className='pt-[18px] sm:pt-[14px]'>The problem described by me is unpopular and very unpleasant for medium and large sites. However, it is solved quite easily and quickly and moreover gives a boost to your site. I did not describe the solution to this problem, as suggested by Google - through robots file, because, I am deeply convinced that this is a very bad way or what they call - "crutch". However, you should always consider this option. Thank you for your attention!</div>
+                    <div className='pt-[18px] sm:pt-[14px]'>The problem described by me is unpopular and very unpleasant for medium and large sites. However, it is solved quite easily and quickly and moreover gives a boost to your site. I did not describe the solution to this problem, as suggested by Google - through robots file, because, I am deeply convinced that this is a very bad way or what they call - &lsquo;crutch&lsquo;. However, you should always consider this option. Thank you for your attention!</div>
                 </div>
             </div>
         </div>
--- src/app/components/ui/BlogCard.component.jsx
+++ src/app/components/ui/BlogCard.component.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, from 'react';
+import React from 'react';
 import Image from "next/image";
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
